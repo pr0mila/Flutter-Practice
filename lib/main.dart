a473b3e5af7433aaa909bf31181d92efab3f3674
Monty <me@promila.info>
2021-09-05 22:07:44 +0600

    images, fonts, container, avatar added

--- lib/main.dart
+++ lib/main.dart
@@ -20,85 +20,52 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.deepPurple,
+        primarySwatch: Colors.teal,
       ),
-      home: MyHomePage(title: 'My Practice'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-
-  final String title;
+  // MyHomePage({Key? key, required this.title}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-enum Animals {cat, dog, fish}
-
-
 class _MyHomePageState extends State<MyHomePage> {
-  Animals _selected = Animals.cat;
-  String _value ="Select an option";
-  List<PopupMenuEntry<Animals>> _items = <PopupMenuEntry<Animals>>[];
-
-  @override
-  void initState() {
-    for(Animals animal in Animals.values) {
-      _items.add(new PopupMenuItem(
-        child: new Text(_getDisplay(animal),),
-        value: animal,
-      ));
-    }
-  }
-
-  void _onSelected(Animals animal) {
-    setState((){
-      _selected = animal;
-      _value = 'You Selected ${_getDisplay(animal)}';
-    });
-  }
-
-  String _getDisplay(Animals animal) {
-    int index = animal.toString().indexOf('.');
-    index++;
-    return animal.toString().substring(index);
-  }
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
-        body: new Container(
-            padding: new EdgeInsets.all(32.0),
-            child: new Center(
-              child: new Row(
-                children: <Widget>[
-                  new Container(
-                    padding: new EdgeInsets.all(5.0),
-                    child: new Text(_value),
-                  ),
-                  new PopupMenuButton<Animals>(
-                      child:  new Icon(Icons.input),
-                      initialValue: Animals.cat,
-                      onSelected: _onSelected,
-                      itemBuilder: (BuildContext context) {
-                        return _items;
-                      }
-                  )
-                ],
-
-
-              ),
-            ))
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/profile.jpg'),
+            ),
+            Text(
+              'Cindrella',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white
+              ) ,
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade100,
+              ) ,
+            )
+          ],
+        ))
         // This trailing comma makes auto-formatting nicer for build methods.
         );
   }
