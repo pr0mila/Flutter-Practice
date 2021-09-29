a10f7235fba450890806fef48b4119d9af9fc059
Monty <me@promila.info>
2021-09-30 01:05:24 +0600

    flatbutton with audio

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,70 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int soundNumber)
+  {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(child: FlatButton(
-              onPressed: (){
-                    final player = AudioCache();
-                    player.play('note1.wav');
-              },
-              child:Text("click me"))),
+          child: Column(
+            children: [
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  playSound(1);
+                },
+                child: Text('1'),
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  playSound(2);
+                },
+                child: Text('2'),
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  playSound(3);
+                },
+                child: Text('3'),
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  playSound(4);
+                },
+                child: Text('4'),
+              ),
+              FlatButton(
+                color: Colors.teal,
+                onPressed: () {
+                  playSound(5);
+                },
+                child: Text('6'),
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  playSound(6);
+                },
+                child: Text('6'),
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {
+                  playSound(7);
+                },
+                child: Text('7'),
+              ),
+            ],
+          ),
         ),
       ),
     );
