ffa6fdad66c96bb00daf270ed4fa171c75461525
Monty <me@promila.info>
2022-01-20 16:19:58 +0600

    networking

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutterintermediate/services/location.dart';
 
 
@@ -16,14 +17,26 @@ class _LoadingScreenState extends State<LoadingScreen> {
     // TODO: implement initState
     super.initState();
     getLocation();
+    getData();
   }
 
   void getLocation() async
   {
-    Location location = Location();
-    await location.getCurrentLocation();
-    print(location.longitude);
-    print(location.latitude);
+  Location location = Location();
+   await location.getCurrentLocation();
+  print(location.longitude);
+   print(location.latitude);
+
+  }
+
+  void getData() async
+  {
+    http.Response response = await http.get(Uri.parse('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=f1adb207ffdbecb6a4cda2b7caf98efd'));
+    print(response.statusCode);
+    if (response.statusCode == 200)
+      print(response.body);
+    else
+      print(response.statusCode);
   }
   @override
   Widget build(BuildContext context) {
@@ -32,9 +45,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: RaisedButton(
           onPressed: () {
             //Get the current location
-            getLocation();
+           // getLocation();
           },
-          child: Text('Get Location'),
+          //child: Text('Get Location'),
         ),
       ),
     );
