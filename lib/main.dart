3e392bf67897fe9977627e6c6e39aaf91c1cf33e
Monty <me@promila.info>
2022-02-07 19:29:25 +0600

    Routes and Hero Animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
-import 'price_screen.dart';
+import 'package:flutterintermediate/screens/welcome_screen.dart';
+import 'package:flutterintermediate/screens/login_screen.dart';
+import 'package:flutterintermediate/screens/registration_screen.dart';
+import 'package:flutterintermediate/screens/chat_screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(FlashChat());
 
-class MyApp extends StatelessWidget {
+class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-          primaryColor: Colors.lightBlue,
-          scaffoldBackgroundColor: Colors.white),
-      home: PriceScreen(),
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: '/',
+      routes: {
+          '/':(context)=> WelcomeScreen(),
+          LoginScreen.id: (context)=> LoginScreen(),
+          RegistrationScreen.id : (context)=> RegistrationScreen(),
+         ChatScreen.id: (context)=> ChatScreen(),
+    },
     );
   }
 }
