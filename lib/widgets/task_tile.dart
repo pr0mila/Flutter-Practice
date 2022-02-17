e33350dac954ddcd57b9334f0d5ba9027897ac53
Monty <me@promila.info>
2022-02-17 13:54:35 +0600

    reformat codes

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TaskTile  extends StatefulWidget {
-
+class TaskTile extends StatefulWidget {
   //const TaskTile ({Key? key, required this.title}) : super(key: key);
   //final String title;
 
@@ -10,41 +9,31 @@ class TaskTile  extends StatefulWidget {
   _TaskTileState createState() => _TaskTileState();
 }
 
-class _TaskTileState  extends State<TaskTile > {
+class _TaskTileState extends State<TaskTile> {
   bool isChecked = false;
-  void checkboxCallback(bool value ) {
-    setState(() {
-      isChecked = value;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-
     return ListTile(
-          title: Text(
-            'this is text',
-            style: TextStyle(
-                decoration: isChecked ? TextDecoration.lineThrough : null),
-          ),
-          trailing:  TextCheckBox(
-            checkboxState: isChecked,
-            toggleCheckbox: (bool value) {
-              checkboxCallback(value);
-            },
-          )
-
-      );
-
-
-
+      title: Text(
+        'this is text',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
+      trailing: TextCheckBox(
+          checkboxState: isChecked,
+          toggleCheckbox: (bool value) {
+            setState(() {
+              isChecked = value;
+            });
+          }),
+    );
   }
-
 }
 
-
 class TextCheckBox extends StatelessWidget {
-  TextCheckBox({required this.checkboxState, required     this.toggleCheckbox});
+  const TextCheckBox(
+      {required this.checkboxState, required this.toggleCheckbox});
   final bool checkboxState;
   final Function(bool value) toggleCheckbox;
   @override
@@ -52,9 +41,8 @@ class TextCheckBox extends StatelessWidget {
     return Checkbox(
         activeColor: Colors.lightBlueAccent,
         value: checkboxState,
-        onChanged: (value){
+        onChanged: (value) {
           toggleCheckbox(value!);
-        }
-    );
+        });
   }
 }
